f97c879fd37c325e152fe4e422767ea05478222e
SharkBSJ <baesungjin1996@gmail.com>
2021-03-14 00:59:29 +0900

    Project Complete

---
+++ README.md
@@ -0,0 +1,9 @@
+화면 캡처 방지 예제 안드로이드 앱
+=============
+
+* 화면 캡처 방지를 위해서 아래의 Android에서 아래 사진처럼 코드를 추가하면 된다.  
+<div style="text-align : center;"><img src="코드.png"></div>
+* 실행화면  
+<div style="text-align : center;"><img src="실행화면.png"></div>
+* 해당 기능 사용 예제  
+<div style="text-align : center;"><img src="예제.png"></div>
---
+++ app/src/main/java/kr/sjsoft/capturepreventionexample/MainActivity.java
@@ -0,0 +1,17 @@
+package kr.sjsoft.capturepreventionexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+    }
+}
